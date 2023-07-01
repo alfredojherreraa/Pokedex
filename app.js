26bb8704dd11d7a3754ef91e69b57f5ef26aac5e
alfredojherreraa <78054100+alfredojherreraa@users.noreply.github.com>
2023-07-01 16:37:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const baseURL = 'https://pokeapi.co/api/v2/pokemon/'
+const pokemon = document.getElementById('pokeName')
+const buttonSearch = document.getElementById('searchPokemon')
+const removePokemon = document.getElementById('borrarPokemon')
+const appNode = document.getElementById('app')
+
+buttonSearch.addEventListener('click', insertarPokemon)
+removePokemon.addEventListener('click', borrarPokemon)
+
+function insertarPokemon(){
+  window.fetch(`${baseURL}${pokemon.value.toLowerCase()}`)
+  .then(response =>{
+    if(response.status ===404){
+      alert('Este pokemon no esta disponible')
+    }else{
+      return response.json()
+    }
+  })
+  .then(responseJSON =>{
+    const allItems =[]
+    const result =[]
+
+    for (let pokemonInfo in responseJSON){
+      result.push([pokemonInfo, responseJSON[pokemonInfo]])
+    }
+
+    console.table(result)
+
+    const pokeImagen = document.createElement('img')
+    pokeImagen.src = result[14][1].front_default 
+    
+    const pokemonName = document.createElement('h1')
+    pokemonName.innerText = `Name: ${result[10][1]} | id: ${result[6][1]}`
+
+    const pokemonType = document.createElement('h3')
+    pokemonType.innerText = `Type: ${result[16][1][0].type.name}`
+
+    const contenedor = document.createElement('section')
+    contenedor.append(pokeImagen, pokemonName, pokemonType)
+
+    allItems.push(contenedor)
+
+    appNode.append(...allItems)
+  })
+}
+
+function borrarPokemon(){
+  let allPokemon = appNode.childNodes 
+  allPokemon = Array.from(allPokemon)
+
+  allPokemon.forEach(pokemons =>{
+    pokemons.remove(pokemons)
+  })
+}
